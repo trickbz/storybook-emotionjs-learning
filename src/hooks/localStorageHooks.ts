c72b828202fb21b37aa237d1fa0084dd6e9eabe6
apetrenko <apetrenko@instartlogic.com>
2020-03-08 08:45:51 +0200

    add typescript support and rename some files to ts / tsx

--- src/hooks/localStorageHooks.ts
+++ src/hooks/localStorageHooks.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useLocalStorage(key, initialValue) {
+export function useLocalStorage(key: string, initialValue: string) {
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.localStorage.getItem(key);
@@ -11,7 +11,7 @@ export function useLocalStorage(key, initialValue) {
     }
   });
 
-  const setValue = value => {
+  const setValue = (value:string|Function) => {
     try {
       const valueToStore = value instanceof Function 
         ? value(storedValue) : 
